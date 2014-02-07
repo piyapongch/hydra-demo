2b1d8ecf9b276485f063d84e936e42f1e14c8ebe
Ubuntu Server 12.04.4 LTS <pcharoen@ubuntu.(none)>
2014-02-07 12:12:06 -0800

    Created a book page model with relationship to the book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,5 @@ class Book < ActiveFedora::Base
   has_attributes :title, datastream: 'descMetadata', multiple: false
   has_attributes :author, datastream: 'descMetadata', multiple: false
 
+  has_many :pages, :property=> :is_part_of
 end
