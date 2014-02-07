5344f160589698c5f11002ca8c48afe6a52bbad2
Piyapong Charoenwattana <piyapong.charoenwattana@ualberta.ca>
2014-02-07 13:10:12 -0800

    Handling multivalued author fields

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ActiveFedora::Base
   has_metadata 'descMetadata', type: BookMetadata
 
   has_attributes :title, datastream: 'descMetadata', multiple: false
-  has_attributes :author, datastream: 'descMetadata', multiple: false
+  has_attributes :author, datastream: 'descMetadata', multiple: true
 
   has_many :pages, :property=> :is_part_of
 end
