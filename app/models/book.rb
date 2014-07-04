62f0e4388d7845c2a0cdd2f125e87ca016944a7f
Ubuntu 12.04.4 Sever <pcharoen@ubuntu.(none)>
2014-07-04 13:52:06 -0600

    Added description field.

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,8 @@ class Book < ActiveFedora::Base
   has_metadata 'descMetadata', type: Datastreams::BookMetadata
 
   has_attributes :title, datastream: 'descMetadata', multiple: false
-  has_attributes :author, datastream: 'descMetadata', multiple: true
+  has_attributes :author, datastream: 'descMetadata', multiple: false
+  has_attributes :description, datastream: 'descMetadata', multiple: false
 
   has_many :pages, :property=> :is_part_of
 end
--- app/models/datastreams/book_metadata.rb
+++ app/models/datastreams/book_metadata.rb
@@ -4,6 +4,7 @@ class Datastreams::BookMetadata < ActiveFedora::OmDatastream
       t.root(path: "fields")
       t.title(index_as: :stored_searchable)
       t.author(index_as: :stored_searchable)
+      t.description(index_as: :stored_searchable)
     end
     
   def self.xml_template
